b679c7c1530dc2ef8176825b4a1c357063e70915
Tash825 <78715311+Tash825@users.noreply.github.com>
2024-12-30 22:56:26 +0530

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,28 @@
+import random
+
+words = ['pikachu','beast','shadow','biker','timepass']
+
+chosen_word = random.choice(words)
+word_display = [ '_' for _ in chosen_word]
+attempts = 8
+
+print("Welcome to hangman")
+
+while attempts>0 and '_' in word_display:
+    print("\n" + ' '.join(word_display))
+    guess = input("Guess the letter: ").lower()
+    if guess in chosen_word:
+        for index, letter in enumerate(chosen_word):
+            if letter == guess:
+                word_display[index] = guess
+    else:
+        print("That letter doesn't appear in the word!")
+        attempts -= 1
+
+if '_' not in word_display:
+    print("You guessed the word!")
+    print(' '.join(word_display))
+    print("You survived")
+else:
+     print("You ran out of attempts. The chosen word was: "+ chosen_word)
+     print("You died")
